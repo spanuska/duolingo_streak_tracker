ebc0d1073e7b44a7652a7cff9f0ffe95a28ee903
Skylar Panuska <skylar.panuska@gmail.com>
2015-04-05 12:25:27 -0400

    adds classification methods to user, adds user index view

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
 
 	attr_accessor :api_hash
 
+#-- CLASS METHODS
 	def self.populate(duo_username)
 		api_hash = self.api_call(duo_username)
 		binding.pry
@@ -20,6 +21,27 @@ class User < ActiveRecord::Base
 		JSON.load(open(url))
 	end
 
+  def self.platinum
+    self.where('streak >= 730')
+  end
+
+  def self.gold
+    self.where(streak: 500..729)
+  end
+
+  def self.silver
+    self.where(streak: 400..499)
+  end
+
+  def self.bronze
+    self.where(streak: 365..399)
+  end
+
+  def self.soon_to_be
+    self.where(streak: 300..364)
+  end
+
+#-- INSTANCE METHODS
 	def create_language_progresses(api_hash)
 		api_hash["languages"].collect do |language|
 			if language["learning"]
