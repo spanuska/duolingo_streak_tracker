f148e152bb8bede0bd4852615b45ac6daa693873
Skylar Panuska <skylar.panuska@gmail.com>
2015-04-04 22:33:32 -0400

    creates and/or updates a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,20 @@ class User < ActiveRecord::Base
 
 	attr_accessor :api_hash
 
-	def populate(duo_username)
-		api_call(duo_username)
+	def self.populate(duo_username)
+		api_hash = self.api_call(duo_username)
 		binding.pry
-		self.find_or_create_by(name: api_hash["username"])
-		self.update(streak: api_hash["site_streak"])
-		create_language_progresses
+		current = self.find_or_create_by(name: api_hash["username"])
+		current.update(streak: api_hash["site_streak"])
+		current.create_language_progresses(api_hash)
 	end
 	
-	def api_call(duo_username)
+	def self.api_call(duo_username)
 		url = "https://api.duolingo.com/users/#{duo_username}"
-		self.api_hash = JSON.load(open(url))
+		JSON.load(open(url))
 	end
 
-	def create_language_progresses
+	def create_language_progresses(api_hash)
 		api_hash["languages"].collect do |language|
 			if language["learning"]
 				self.language_progresses.create(
