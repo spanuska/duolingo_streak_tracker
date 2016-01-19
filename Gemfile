62ec533d02d83648fdc2fa710b5ea2a889ba9344
Skylar Panuska <skylar.panuska@gmail.com>
2016-01-19 15:58:49 -0500

    updates gems, adds scopes to user model to replace class methods

--- Gemfile
+++ Gemfile
@@ -36,7 +36,8 @@ group :development, :test do
   gem 'web-console', '~> 2.0'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'spring'
+  # gem 'spring'
+  gem 'pry-rails'
 end
 
 group :test, :development do
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,12 @@ class User < ActiveRecord::Base
 	validates :name, uniqueness: true
 	has_many :language_progresses
 
+	scope :platinum, -> { where('streak >= 730') }
+	scope :gold, -> { where(streak: 500..729) }
+	scope :silver, -> { where(streak: 400..499) }
+	scope :bronze, -> { where(streak: 365..399) }
+	scope :soon_to_be, -> { where(streak: 300..364) }
+
 	attr_accessor :api_hash
 
 	def populate_from_refresh(api_hash)
@@ -31,27 +37,12 @@ class User < ActiveRecord::Base
     self.where('streak >= 730')
   end
 
-  def self.gold
-    self.where(streak: 500..729)
-  end
-
-  def self.silver
-    self.where(streak: 400..499)
-  end
-
-  def self.bronze
-    self.where(streak: 365..399)
-  end
-
-  def self.soon_to_be
-    self.where(streak: 300..364)
-  end
 
 #-- INSTANCE METHODS
 	def create_language_progresses(api_hash)
 		api_hash["languages"].collect do |language|
 			if language["learning"]
-				self.language_progresses.create(
+				self.language_progresses.create( #change this to find_or_create_by username, then 
 						learning: language["learning"],
 						name: language["language_string"],
 						level: language["level"],
