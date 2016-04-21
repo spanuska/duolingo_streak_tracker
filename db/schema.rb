3022a3f6064804f149f40faea55412cfd046c872
Skylar Panuska <skylar.panuska@gmail.com>
2016-04-21 10:13:44 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160313005853) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "languages", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
